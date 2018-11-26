749128eea65d910eedbdf9e027b8ef3d05fd1f59
Katica Rankovic <katicar@outlook.com>
2018-11-26 19:39:05 +0100

    first website

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+window.onload = function() {
+    // var basePath = window.location;
+    // console.log(basePath);
+    document.getElementById("bars").addEventListener("click", function(){
+        document.getElementById("overlay").classList.toggle("h-100");
+        document.getElementById("bars").classList.toggle( "fa-times");
+    });
+    var flag=0;
+    document.getElementById("search").addEventListener("click", function(){
+        document.getElementById("inputSearch").style.width="200px";
+        document.getElementById("inputSearch").focus();
+        flag++;
+        if(flag%2==0){
+            document.getElementById("inputSearch").style.width="0px";
+        }
+    });
+
+    var menuLinks=[{name: "Home", path: "index.html"}, {name: "Reviews", path: "assets/pages/reviews.html"}, {name: "About Us", path: "assets/pages/aboutus.html"},{name: "About Me", path: "assets/pages/aboutme.html"}];
+    var menuLinksP=[{name: "Home", path: "./index.html"}, {name: "Reviews", path: "reviews.html"}, {name: "About Us", path: "aboutus.html"},{name: "About Me", path: "aboutme.html"}];
+
+    
+    renderNavigation();
+    function renderNavigation() {
+        var getUrl = location.pathname;
+        console.log(getUrl);
+        var html="";
+        html+="<ul>";
+        if(getUrl = "index.html"){
+        for(index in menuLinks){ 
+            html+="<li><a href='"+menuLinks[index].path+"'>"+menuLinks[index].name+"</a></li>";
+        }}
+        else{
+            for(index in menuLinksP){
+                html+="<li><a href='"+menuLinksP[index].path+"'>"+menuLinksP[index].name+"</a></li>";
+            }
+        }
+        html+="</ul>";
+        document.getElementById("overlay").innerHTML=html;
+    }
+}
